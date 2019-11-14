3cfb89f1b7856c95023b83943e218698601d5bd0
Szymon Pilkowski <s.pilkowski@molecule.one>
2019-11-14 20:21:17 +0100

    Generic centroid function

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@ extern crate num_traits;
 use rand::distributions::{IndependentSample, Range};
 use std::cmp::Ordering;
 use std::ops::{Sub};
+use std::iter::Sum;
 use num_traits::sign::Signed;
+use num_traits::{FromPrimitive, ToPrimitive};
+
 
 #[derive(Debug)]
 pub struct Group {
-    center: f32,
+    center: f64,
     items: Vec<f32>,
 }
 
@@ -14,7 +17,7 @@ impl Group {
     fn add(&mut self, item: f32) {
         self.items.push(item);
     }
-    fn set_center(&mut self, center: f32) {
+    fn set_center(&mut self, center: f64) {
         self.center = center;
     }
     fn reset(&mut self) {
@@ -22,10 +25,19 @@ impl Group {
     }
 }
 
-fn centroid(data: &[f32]) -> f32 {
-    let sum: f32 = data.iter().sum();
-    let length = data.iter().len();
-    sum / length as f32
+fn centroid<'a, T: 'a>(numbers: &'a [T]) -> Option<f64>
+where
+    T: ToPrimitive + Sum<&'a T>,
+{
+    match numbers.len() {
+        0 => None,
+        len => {
+            let sum = numbers.iter().sum::<T>();
+            let length = f64::from_usize(len)?;
+
+            T::to_f64(&sum).map(|sum| sum / length)
+        }
+    }
 }
 
 fn distance<T: Sub<Output = T> + Signed>(a: T, b: T) -> T {
@@ -36,8 +48,8 @@ fn closest<'a>(groups: &'a mut Vec<Group>, target: f32) -> &'a mut Group {
     let closest = groups
         .iter_mut()
         .min_by(|a, b| {
-            let da = distance(a.center, target);
-            let db = distance(b.center, target);
+            let da = distance(a.center, target as f64);
+            let db = distance(b.center, target as f64);
             da.partial_cmp(&db).unwrap_or(Ordering::Equal)
         })
         .unwrap();
@@ -51,7 +63,7 @@ fn qualify(el: f32, mut groups: &mut Vec<Group>) {
 
 fn update_centroid(group: &mut Group) {
     if group.items.len() > 0 {
-        let centroid = centroid(&group.items);
+        let centroid = centroid(&group.items).unwrap();
         group.set_center(centroid);
     }
 }
@@ -72,7 +84,7 @@ pub fn solve(data: &Vec<f32>, groups_count: i32, epochs: i32) -> Vec<Group> {
     for _ in 0..groups_count {
         let center = range.ind_sample(&mut rng);
         let group = Group {
-            center: center as f32,
+            center: center as f64,
             items: Vec::new(),
         };
         groups.push(group);
