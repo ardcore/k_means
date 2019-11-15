ca041f1a3139e016c5a170de1849e8a94ce579fe
Szymon Pilkowski <s.pilkowski@molecule.one>
2019-11-15 01:21:26 +0100

    Make everything generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,23 @@
 extern crate num_traits;
 use rand::distributions::{IndependentSample, Range};
-use std::cmp::Ordering;
-use std::ops::{Sub};
+use std::cmp::{Ordering, PartialOrd};
+use std::ops::{Sub, Add};
 use std::iter::Sum;
 use num_traits::sign::Signed;
 use num_traits::{FromPrimitive, ToPrimitive};
+use std::marker::Copy;
 
+pub trait NumberLike: PartialOrd + ToPrimitive + Sum + Add + Copy {}
+impl<T: PartialOrd + ToPrimitive + Sum + Add + Copy> NumberLike for T {}
 
 #[derive(Debug)]
-pub struct Group {
+pub struct Group<T: NumberLike> {
     center: f64,
-    items: Vec<f32>,
+    items: Vec<T>,
 }
 
-impl Group {
-    fn add(&mut self, item: f32) {
+impl<T: NumberLike> Group<T> {
+    fn add(&mut self, item: T) {
         self.items.push(item);
     }
     fn set_center(&mut self, center: f64) {
@@ -27,15 +30,17 @@ impl Group {
 
 fn centroid<'a, T: 'a>(numbers: &'a [T]) -> Option<f64>
 where
-    T: ToPrimitive + Sum<&'a T>,
+    T: NumberLike,
 {
     match numbers.len() {
         0 => None,
         len => {
-            let sum = numbers.iter().sum::<T>();
+            let mut sum: f64 = 0.0;
+            for n in numbers {
+                sum = sum + T::to_f64(n).unwrap();
+            }
             let length = f64::from_usize(len)?;
-
-            T::to_f64(&sum).map(|sum| sum / length)
+            Some(sum / length)
         }
     }
 }
@@ -44,47 +49,52 @@ fn distance<T: Sub<Output = T> + Signed>(a: T, b: T) -> T {
     (a - b).abs()
 }
 
-fn closest<'a>(groups: &'a mut Vec<Group>, target: f32) -> &'a mut Group {
+fn closest<'a, T: NumberLike>(groups: &'a mut Vec<Group<T>>, target: &T) -> &'a mut Group<T> {
     let closest = groups
         .iter_mut()
         .min_by(|a, b| {
-            let da = distance(a.center, target as f64);
-            let db = distance(b.center, target as f64);
+            let da = distance(a.center, T::to_f64(&target).unwrap());
+            let db = distance(b.center, T::to_f64(&target).unwrap());
             da.partial_cmp(&db).unwrap_or(Ordering::Equal)
         })
         .unwrap();
     closest
 }
 
-fn qualify(el: f32, mut groups: &mut Vec<Group>) {
-    let closest_group = closest(&mut groups, el);
+fn qualify<T: NumberLike>(el: T, mut groups: &mut Vec<Group<T>>) {
+    let closest_group = closest(&mut groups, &el);
     closest_group.add(el);
 }
 
-fn update_centroid(group: &mut Group) {
+fn update_centroid<T: NumberLike>(group: &mut Group<T>) {
     if group.items.len() > 0 {
         let centroid = centroid(&group.items).unwrap();
         group.set_center(centroid);
     }
 }
 
-pub fn solve(data: &Vec<f32>, groups_count: i32, epochs: i32) -> Vec<Group> {
+pub fn solve<T: NumberLike>(data: &Vec<T>, groups_count: i32, epochs: i32) -> Vec<Group<T>> {
     let max = data
         .iter()
-        .max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+        .max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+        .unwrap();
     let min = data
         .iter()
-        .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+        .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+        .unwrap();
 
-    let range = Range::new(*min.unwrap() as u32, *max.unwrap() as u32);
+    let min = T::to_f64(&min).unwrap();
+    let max = T::to_f64(&max).unwrap();
+
+    let range = Range::new(min, max);
     let mut rng = rand::thread_rng();
 
-    let mut groups: Vec<Group> = Vec::new();
+    let mut groups: Vec<Group<T>> = Vec::new();
 
     for _ in 0..groups_count {
         let center = range.ind_sample(&mut rng);
         let group = Group {
-            center: center as f64,
+            center,
             items: Vec::new(),
         };
         groups.push(group);
