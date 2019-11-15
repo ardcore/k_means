f0cd0c4c9cfb9349315e1b997eb6b5573aa2e6c8
Szymon Pilkowski <s.pilkowski@molecule.one>
2019-11-15 02:40:40 +0100

    Update examples

--- examples/kmeans_example/src/main.rs
+++ examples/kmeans_example/src/main.rs
@@ -2,11 +2,14 @@ extern crate k_means;
 
 fn main() {
     let data = vec![
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        1, 2, 3, 4, 7, 8, 9, 10, 13, 14, 15, 17, 18, 19, 20,
     ];
     let data_float: Vec<f32> = data.iter().map(|v| *v as f32).collect();
-    let result = k_means::solve(&data, 4, 100);
-    let result_float = k_means::solve(&data_float, 4, 100);
-    println!("result: {:?}", result);
-    println!("result_float: {:?}", result_float);
+    let data_neg: Vec<i32> = data.iter().map(|v| { if v % 2 == 0 { *v } else { -*v }}).collect();
+    let result = k_means::solve(&data, 4, 150);
+    let result_float = k_means::solve(&data_float, 4, 150);
+    let result_with_neg = k_means::solve(&data_neg, 4, 150);
+    println!("result: {:?}\n", result);
+    println!("result_float: {:?}\n", result_float);
+    println!("result_with_neg: {:?}\n", result_with_neg);
 }
